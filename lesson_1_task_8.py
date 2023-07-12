3de5350203b1c1e789ab0f02929d5ad373c01b55
mjdemo9 <125508433+mjdemo9@users.noreply.github.com>
2023-07-13 03:08:57 +0900

    Update lesson_1_task_8.py

--- lesson_1_task_8.py
+++ lesson_1_task_8.py
@@ -1,12 +1,14 @@
-num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0] #88005553535
-print (num [7])
-print (num [7])
-print (num [-1])
-print (num [-1])
-print (num [4])
-print (num [4])
-print (num [4])
-print (num [2])
-print (num [4])
-print (num [2])
-print (num [4])
+def print_num():
+    num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0] #88005553535
+
+print(8)
+print(8)
+print(0)
+print(0)
+print(5)
+print(5)
+print(5)
+print(3)
+print(5)
+print(3)
+print(5)
